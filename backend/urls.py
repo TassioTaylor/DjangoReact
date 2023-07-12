27e3581f2c3ae8eca2e4f3c1908365bc3bd521ea
TassioTaylor <tassiotaylor@gmail.com>
2023-07-11 21:33:48 -0300

    Adicionando querset User

--- backend/urls.py
+++ backend/urls.py
@@ -3,11 +3,13 @@ from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from django.contrib import admin
 from django.urls import path
+from core.views import ListViewSet, ItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
- 
+router.register(r'list', ListViewSet) 
+router.register(r'item', ItemViewSet) 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from core.models import List, Item
+from rest_framework import viewsets, authentication
+from rest_framework import permissions
+from .serializers import ListSerializer, ItemSerializer
 
-# Create your views here.
+
+class ListViewSet(viewsets.ModelViewSet):
+    
+    queryset = List.objects.all().order_by()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+    def get_queryset(self):
+        user = self.request.user
+        return List.objects.filter(owner=user)
+    
+class ItemViewSet(viewsets.ModelViewSet):
+    
+    queryset = Item.objects.all().order_by()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication,  authentication.SessionAuthentication]
