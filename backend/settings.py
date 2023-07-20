cf0566776ce01cd9b697c4ee7a3c5823d236290a
TassioTaylor <tassiotaylor@gmail.com>
2023-07-20 15:39:10 -0300

    Alteraçoes de Rotas

--- backend/settings.py
+++ backend/settings.py
@@ -143,3 +143,12 @@ CORS_ALLOW_METHODS = (
 )
 
 CORS_ALLOW_ALL_ORIGINS = True 
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+   ),
+}
--- backend/urls.py
+++ backend/urls.py
@@ -4,15 +4,17 @@ from .views import UserViewSet, GroupViewSet
 from django.contrib import admin
 from django.urls import path
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'list', ListViewSet) 
+router.register(r'list', ListViewSet, basename='list') 
 router.register(r'item', ItemViewSet) 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', views.obtain_auth_token, name = 'api-token-auth'),
 ]
